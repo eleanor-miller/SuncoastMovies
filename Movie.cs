55f52e2cbb1429cc372914ad4cf83bc95d399d24
Eleanor Miller <eleanormiller.dev@gmail.com>
2021-09-22 20:55:12 -0400

    initial commit

---
+++ Movie.cs
@@ -0,0 +1,10 @@
+using System;
+namespace SuncoastMovies
+{
+class Movie {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string PrimaryDirector { get; set; }
+    public int YearReleased { get; set; }
+    public string Genre { get; set; }
+} }
